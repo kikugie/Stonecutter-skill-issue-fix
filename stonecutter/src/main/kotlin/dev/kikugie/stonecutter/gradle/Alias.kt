25e29ad42ca38357f550d95abcab9cac14579e88
Kikugie <kikugie@duck.com>
2024-06-03 23:51:28 +0200

    provide alias for StonecutterSettings

---
+++ stonecutter/src/main/kotlin/dev/kikugie/stonecutter/gradle/Alias.kt
@@ -0,0 +1,11 @@
+@file:Suppress("unused")
+
+package dev.kikugie.stonecutter.gradle
+
+import dev.kikugie.stonecutter.StonecutterSettings
+
+/**
+ * Used to avoid import errors when migrating from 0.3 to 0.4.
+ */
+@Deprecated("Replace import with 'import dev.kikugie.stonecutter.StonecutterSettings'")
+typealias StonecutterSettings = StonecutterSettings
