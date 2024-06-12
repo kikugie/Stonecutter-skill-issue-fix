35ee6a46abeb2d9f05b1fe89c67f227207c61697
Kikugie <kikugie@duck.com>
2024-06-12 15:33:11 +0200

    Add StonecutterProject documentation and make isActive mutable

--- stonecutter/src/main/kotlin/dev/kikugie/stonecutter/StonecutterProject.kt
+++ stonecutter/src/main/kotlin/dev/kikugie/stonecutter/StonecutterProject.kt
@@ -1,9 +1,34 @@
 package dev.kikugie.stonecutter
 
-data class StonecutterProject internal constructor(
+import dev.kikugie.semver.SemanticVersion
+
+class StonecutterProject internal constructor(
+    /**
+     * The name of this project's directory, as in `versions/${project}`.
+     */
     val project: ProjectName,
+    /**
+     * The assigned version of this project. Must be a valid [SemanticVersion].
+     * By default, its equal to [project], unless assigned by using `vers()` in the project settings.
+     */
     val version: TargetVersion,
-    val isActive: Boolean = false,
+    isActive: Boolean = false
 ) {
-    internal fun asActive() = StonecutterProject(project, version, true)
+    /**
+     * Whenever this project is selected as active.
+     */
+    var isActive: Boolean = isActive
+        internal set
+
+    internal fun asActive() = this.also { isActive = true }
+
+    override fun equals(other: Any?): Boolean =
+        if (other !is StonecutterProject) false
+        else project == other.project && version == other.version
+
+    override fun hashCode(): Int {
+        var result = project.hashCode()
+        result = 31 * result + version.hashCode()
+        return result
+    }
 }
