5adea8ad5dfbd81e79e44ddc80bf8daa63be3e3f
Kikugie <kikugie@duck.com>
2024-07-07 12:45:39 +0200

    Sheep say baaa to tell me how baaa-d gradle is

--- fletching_table/src/main/kotlin/dev/kikugie/fletching_table/impl/StonecutterGradle.kt
+++ fletching_table/src/main/kotlin/dev/kikugie/fletching_table/impl/StonecutterGradle.kt
@@ -3,16 +3,21 @@ package dev.kikugie.fletching_table.impl
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.externalSystem.model.DataNode
 import com.intellij.openapi.externalSystem.model.project.ProjectData
+import com.intellij.openapi.module.Module
 import com.intellij.openapi.module.ModuleManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManager
 import com.intellij.openapi.roots.ModuleRootManager
 import dev.kikugie.stonecutter.configuration.StonecutterModel
+import dev.kikugie.stonecutter.process.decode
+import dev.kikugie.stonecutter.process.runIgnoring
 import org.jetbrains.plugins.gradle.model.ExternalProject
 import org.jetbrains.plugins.gradle.service.project.AbstractProjectResolverExtension
 import org.jetbrains.plugins.gradle.service.project.data.ExternalProjectDataCache
 import java.io.File
-import com.intellij.openapi.module.Module
+import java.util.WeakHashMap
+import kotlin.io.path.isReadable
+import kotlin.io.path.notExists
 
 class ReloadListener : AbstractProjectResolverExtension() {
     @Suppress("UnstableApiUsage")
@@ -23,7 +28,7 @@ class ReloadListener : AbstractProjectResolverExtension() {
 
 @Service(Service.Level.PROJECT)
 class StonecutterService(private val root: Project) {
-    private val models: MutableMap<Module, StonecutterModel> = mutableMapOf()
+    private val models: MutableMap<Module, StonecutterModel> = WeakHashMap()
 
     init {
         reload()
@@ -40,23 +45,21 @@ class StonecutterService(private val root: Project) {
         }
 
         for (module in manager.modules) module.getComponent(ModuleRootManager::class.java).contentRoots.forEach {
-            val child = cache.getRootExternalProject(it.path) ?: return@forEach
-            loadProject(child, modules)
+            val project = cache.getRootExternalProject(it.path) ?: return@forEach
+            val buildFileName = project.buildFile?.name
+            if (buildFileName == "stonecutter.gradle" || buildFileName == "stonecutter.gradle.kts")
+                loadVersions(project, modules)
         }
     }
 
-    private fun loadProject(project: ExternalProject, modules: Map<File, Module>) {
-        try {
-            val buildFile = project.buildFile
-            if (buildFile?.name != "stonecutter.gradle" && buildFile?.name != "stonecutter.gradle.kts")
-                throw Exception()
-            loadVersions(project, modules)
-        } catch (_: Exception) {
+    private fun loadVersions(project: ExternalProject, modules: Map<File, Module>) {
+        for (it in project.childProjects.values) {
+            val module = modules[it.projectDir] ?: continue
+            val file = it.buildDir.resolve("stonecutterCache/model.bin").toPath()
+            if (file.notExists() || !file.isReadable()) continue
+            runIgnoring {
+                models[module] = file.decode()
+            }
         }
     }
-
-    private fun loadVersions(project: ExternalProject, modules: Map<File, Module>) =
-        project.childProjects.values.forEach {
-            val file = it.buildDir.resolve("stonecutterCache/model.bin")
-        }
 }
