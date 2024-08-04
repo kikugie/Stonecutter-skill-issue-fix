1a74f69648e0f18d96d1beed058037218e4f382d
Kikugie <kikugie@duck.com>
2024-08-04 21:32:02 +0200

    Import thingy

--- intellij/src/main/kotlin/dev/kikugie/stonecutter/intellij/util/ParserUtils.kt
+++ intellij/src/main/kotlin/dev/kikugie/stonecutter/intellij/util/ParserUtils.kt
@@ -1,16 +1,11 @@
 package dev.kikugie.stonecutter.intellij.util
 
-import com.intellij.openapi.module.Module
 import dev.kikugie.stitcher.data.component.Definition
-import dev.kikugie.stitcher.data.token.MarkerType
+import dev.kikugie.stitcher.exception.StoringErrorHandler
 import dev.kikugie.stitcher.lexer.Lexer
 import dev.kikugie.stitcher.parser.CommentParser
-import dev.kikugie.stitcher.eval.ConditionChecker
-import dev.kikugie.stitcher.eval.isNotEmpty
-import dev.kikugie.stitcher.exception.StoringErrorHandler
 import dev.kikugie.stonecutter.configuration.StonecutterModel
 import dev.kikugie.stonecutter.process.toParams
-import org.jetbrains.plugins.gradle.service.project.data.ExternalProjectDataCache
 
 fun String.parse(model: StonecutterModel): Definition? {
     val lexer = Lexer(this)
@@ -22,15 +17,4 @@ fun String.parse(): Definition? {
     val lexer = Lexer(this)
     val parser = CommentParser(lexer, StoringErrorHandler())
     return parser.parse()
-}
-
-fun String.findMatching(module: Module): List<StonecutterModel> {
-    val service = module.stonecutterService ?: return emptyList()
-    val def = parse()
-        ?.takeIf { it.type == MarkerType.CONDITION && it.isNotEmpty() }
-        ?: return emptyList()
-    return service.models.values.filter {
-        val visitor = ConditionChecker(it.data.toParams(it.current.version))
-        visitor.visitDefinition(def)
-    }
 }
