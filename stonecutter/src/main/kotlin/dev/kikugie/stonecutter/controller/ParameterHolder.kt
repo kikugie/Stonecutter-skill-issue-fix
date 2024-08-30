22fd26f1024964a7bfc982f1ca73218af5fee035
kikugie <stormikiku@gmail.com>
2024-08-30 17:27:57 +0200

    Simplify ParameterHolder

--- stonecutter/src/main/kotlin/dev/kikugie/stonecutter/controller/ParameterHolder.kt
+++ stonecutter/src/main/kotlin/dev/kikugie/stonecutter/controller/ParameterHolder.kt
@@ -1,17 +1,24 @@
 package dev.kikugie.stonecutter.controller
 
+import dev.kikugie.stonecutter.ProjectName
 import dev.kikugie.stonecutter.StonecutterProject
+import dev.kikugie.stonecutter.StonecutterUtility
+import dev.kikugie.stonecutter.TargetVersion
 import dev.kikugie.stonecutter.build.BuildConfiguration
 
 /**
  * Stores parameters configured in [StonecutterController.parameters].
  *
  * @property branch Currently processed branch
- * @property version Currently processed version.
- * **May not exist in the given branch**, but you should still provide the same set of parameters.
+ * @property metadata Currently processed version.
+ * **May not exist in the given branch**, but you should still provide the same set of parameters
  */
 @Suppress("MemberVisibilityCanBePrivate")
 class ParameterHolder(
     val branch: ProjectBranch,
-    val version: StonecutterProject
-) : BuildConfiguration(branch)
+    val metadata: StonecutterProject
+) : BuildConfiguration(branch), StonecutterUtility {
+    val project: ProjectName get() = metadata.project
+    val version: TargetVersion get() = metadata.version
+    val isActive: Boolean get() = metadata.isActive
+}
