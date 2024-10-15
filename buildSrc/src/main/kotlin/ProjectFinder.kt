2032917f91c994abbb19e89896a7b215bcac22cb
kikugie <stormikiku@gmail.com>
2024-10-15 18:20:17 +0200

    Add hall of fame updates from 0.4

--- buildSrc/src/main/kotlin/ProjectFinder.kt
+++ buildSrc/src/main/kotlin/ProjectFinder.kt
@@ -1,16 +1,24 @@
 import com.charleskorn.kaml.Yaml
 import com.charleskorn.kaml.decodeFromStream
-import com.github.ajalt.mordant.rendering.TextColors
+import com.github.ajalt.mordant.rendering.TextColors.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.runBlocking
 import java.io.File
 
 object ProjectFinder {
+    private const val EXCLUDE = "EXCLUDE"
+    private val OK = green("OK")
+    private val FIX = brightYellow("FX")
+    private val CUSTOM = cyan("CT")
+    private val EXCLUDED = gray("EX")
+    private val ERROR = red("ER")
+
     fun find(file: File): String =
         find(file.inputStream().use { Yaml.default.decodeFromStream<SearchEntries>(it) }.entries)
 
     fun find(entries: List<SearchEntry>): String = runBlocking {
+        println(brightGreen("Searching ${entries.size} entries..."))
         val longest = entries.maxOf { it.name.length }
         val template = "| %s | %s | %s | %s |"
         val header = "| ${"Name".padEnd(longest)} | MR | CF | GH |"
@@ -21,8 +29,8 @@ object ProjectFinder {
 
         val flow = entries.asFlow().flowOn(Dispatchers.Default).transform {
             emit(ProjectInfo.Builder().apply {
-                CurseforgeAPI.get(it)?.invoke(this)
-                ModrinthAPI.get(it)?.invoke(this)
+                if (it.curseforge != EXCLUDE) CurseforgeAPI.get(it)?.invoke(this)
+                if (it.modrinth != EXCLUDE) ModrinthAPI.get(it)?.invoke(this)
 
                 it.title?.let { title = it }
                 it.icon?.let { icon = it }
@@ -30,10 +38,11 @@ object ProjectFinder {
                 downloads = (downloads + it.adjust).coerceAtMost(Int.MAX_VALUE)
 
                 fun Any?.status(replaced: Any?) = when {
-                    this != null && replaced != null -> TextColors.brightYellow("FX")
-                    this == null && replaced == null -> TextColors.red("NO")
-                    this == null && replaced != null -> TextColors.cyan("OT")
-                    else -> TextColors.green("OK")
+                    replaced == EXCLUDE -> EXCLUDED
+                    this != null && replaced != null -> FIX
+                    this == null && replaced == null -> ERROR
+                    this == null && replaced != null -> CUSTOM
+                    else -> OK
                 }
                 println(
                     template.format(
@@ -44,15 +53,17 @@ object ProjectFinder {
                     )
                 )
 
-                it.modrinth?.let { modrinth = it }
-                it.curseforge?.let { curseforge = it }
+                it.modrinth.takeUnless { it == EXCLUDE }?.let { modrinth = it }
+                it.curseforge.takeUnless { it == EXCLUDE }?.let { curseforge = it }
                 it.github?.let { github = it }
             }.tryBuild().getOrNull())
         }.onCompletion {
             println(divider)
         }.filterNotNull()
 
+
         val projects = flow.toList()
+            .also { println(brightGreen("Sorting ${entries.size} entries...")) }
             .sortedBy { -it.downloads }
             .joinToString(",\n") { it.toJS() }
         projects
